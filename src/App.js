39cf56faaa3ba109d9fef6f9a8bcdb4888afbb95
Satabdi Sobhan <83968933+Satabdi08@users.noreply.github.com>
2023-03-09 13:48:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       setPredictionLoading(true)
       setModelPrediction(null)
       setRequestFailed(null)
-      axios.post("http://satabdisobhanpradhan.pythonanywhere.com/get_prediction", selectedObject)
+      axios.post('http://satabdisobhanpradhan.pythonanywhere.com/get_prediction', selectedObject)
       .then(function (response) {
         setPredictionLoading(false)
         setModelPrediction(response.data.result)
